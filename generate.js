0de5aff34057ef646c3bec22497e9b53cc10b9fb
vadimzavin-dev <vadim.zavin@gmail.com>
2026-05-23 18:27:45 -0700

    Update generate.js

--- generate.js
+++ generate.js
@@ -1,7 +1,8 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   if (req.method === 'OPTIONS') return res.status(200).end();
   if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
 
@@ -23,4 +24,4 @@ export default async function handler(req, res) {
   } catch (err) {
     return res.status(500).json({ error: err.message });
   }
-}
+};
